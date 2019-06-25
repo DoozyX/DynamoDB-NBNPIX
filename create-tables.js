b9038ed3b72b5471a33f29b6cc6dc58485d7a823
Doozy <slobodan_kletnikov@hotmail.com>
2019-06-25 23:26:52 +0200

    Add tables definition for auctions data set

---
+++ create-tables.js
@@ -0,0 +1,97 @@
+var AWS = require("aws-sdk");
+    
+AWS.config.update({
+  region: "localhost",
+  endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var ebay = {
+    TableName : "ebay",
+    KeySchema: [       
+        { AttributeName: "id", KeyType: "HASH"},  //Partition key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "id", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+var yahoo = {
+    TableName : "yahoo",
+    KeySchema: [       
+        { AttributeName: "id", KeyType: "HASH"},  //Partition key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "id", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+var ubid = {
+    TableName : "ubid",
+    KeySchema: [       
+        { AttributeName: "id", KeyType: "HASH"},  //Partition key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "id", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+var t321gone = {
+    TableName : "t321gone",
+    KeySchema: [       
+        { AttributeName: "id", KeyType: "HASH"},  //Partition key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "id", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(ebay, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
+dynamodb.createTable(yahoo, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
+
+dynamodb.createTable(ubid, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
+dynamodb.createTable(t321gone, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});

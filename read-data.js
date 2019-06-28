03aa03bb38e6d5ea93483c4ff4c3219464bb9693
Doozy <slobodan_kletnikov@hotmail.com>
2019-06-28 08:52:02 +0200

    Add query to get single item

--- read-data.js
+++ read-data.js
@@ -3,30 +3,38 @@ const AWS = require("aws-sdk");
 require("./awsconfig");
 
 const docClient = new AWS.DynamoDB();
-let params = {
-    TableName: 'ebay',
-    Select: 'ALL_ATTRIBUTES', // optional (ALL_ATTRIBUTES | ALL_PROJECTED_ATTRIBUTES | 
-    //           SPECIFIC_ATTRIBUTES | COUNT)
-    ConsistentRead: false, // optional (true | false)
-};
-let start = process.hrtime.bigint();
-docClient.scan(params, function (err, data) {
-    if (err) console.log(err); // an error occurred
-    else console.log(JSON.stringify(data)); // successful response
-    const end = process.hrtime.bigint();
-    console.log(`Scan1 took ${(end-start) / 1000000n} ms`)
-});
-
-params = {
-    TableName: 'ebay',
-    Select: 'ALL_ATTRIBUTES', // optional (ALL_ATTRIBUTES | ALL_PROJECTED_ATTRIBUTES | 
-    //           SPECIFIC_ATTRIBUTES | COUNT)
-    ConsistentRead: false, // optional (true | false)
-};
-start = process.hrtime.bigint();
-docClient.scan(params, function (err, data) {
-    if (err) console.log(err); // an error occurred
-    else console.log(JSON.stringify(data)); // successful response
-    const end = process.hrtime.bigint();
-    console.log(`Scan1 took ${(end-start) / 1000000n} ms`)
-});
+(async () => {
+    try {
+        {
+            const params = {
+                TableName: 'ebay',
+                Select: 'ALL_ATTRIBUTES',
+                ConsistentRead: false,
+                Limit: 5,
+            };
+            const start = process.hrtime.bigint();
+            const data = await docClient.scan(params).promise();
+            console.log(JSON.stringify(data)); // successful response
+            const end = process.hrtime.bigint();
+            console.log(`Scan1 took ${(end - start) / 1000000n} ms`)
+        }
+        {
+            const params = {
+                TableName: 'ebay',
+                AttributesToGet: ["item"],
+                Key: {
+                    id: {
+                        S: "31e8e3a0-63da-4bad-8f15-cb7d34f99eeb"
+                    }
+                }
+            };
+            const start = process.hrtime.bigint();
+            const data = await docClient.getItem(params).promise();
+            console.log(JSON.stringify(data)); // successful response
+            const end = process.hrtime.bigint();
+            console.log(`Scan2 took ${(end - start) / 1000000n} ms`)
+        }
+    } catch (e) {
+        console.log(e);
+    }
+})();
